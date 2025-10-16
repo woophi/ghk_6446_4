eb34dcc2ce0f647ad9f89f5ac88b295386f14c76
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-10-16 08:39:24 +0200

    fix

--- src/thx/ThxLayout.tsx
+++ src/thx/ThxLayout.tsx
@@ -4,8 +4,7 @@ import sparkles from '../assets/sparkles.png';
 import { appSt } from '../style.css';
 import { thxSt } from './style.css';
 
-const link =
-  'alfabank://investments/market_asset_card?title=%D0%9D%D0%9E%D0%92%D0%90%D0%A2%D0%AD%D0%9A&id=RU000A0DKVS5&subtitle=NVTK&url=v1%2Fscreen-details-prefiller%2Fproducts%2Fbrokerage%2FSTOCK%2FRU000A0DKVS5&type=STOCK';
+const link = 'alfabank:///';
 
 export const ThxLayout = () => {
   return (
