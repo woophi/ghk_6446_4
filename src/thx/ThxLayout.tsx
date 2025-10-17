d2983634b543729f5a8a00e68e4761c6d219889c
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-10-17 15:06:51 +0200

    fix

--- src/thx/ThxLayout.tsx
+++ src/thx/ThxLayout.tsx
@@ -1,27 +1,17 @@
-import { ButtonMobile } from '@alfalab/core-components/button/mobile';
-import { Typography } from '@alfalab/core-components/typography';
-import sparkles from '../assets/sparkles.png';
-import { appSt } from '../style.css';
+import { Spinner } from '@alfalab/core-components/spinner';
+import { useTimeout } from '../hooks/useTimeout';
 import { thxSt } from './style.css';
 
 const LINK = 'alfabank://longread?endpoint=v1/adviser/longreads/79127';
 
 export const ThxLayout = () => {
+  useTimeout(() => {
+    window.location.replace(LINK);
+  }, 2500);
   return (
     <>
       <div className={thxSt.container}>
-        <img src={sparkles} width={80} height={80} className={thxSt.rocket} />
-        <Typography.TitleResponsive style={{ margin: '24px 0 12px' }} font="system" tag="h1" view="small" weight="medium">
-          Сервис пока недоступен
-        </Typography.TitleResponsive>
-        <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
-          Вся команда очень старается — скоро всё заработает!
-        </Typography.Text>
-      </div>
-      <div className={appSt.bottomBtn}>
-        <ButtonMobile block view="secondary" href={LINK}>
-          Спасибо, понятно!
-        </ButtonMobile>
+        <Spinner style={{ margin: 'auto' }} visible preset={48} />
       </div>
     </>
   );
